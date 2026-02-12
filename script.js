b26ee5df8ed985930e73142c5e8c80bc7ed43df3
Ribin K Karun <ribinkk@users.noreply.github.com>
2026-02-13 03:25:35 +0530

    Fix JS error in scroll listener

--- script.js
+++ script.js
@@ -33,7 +33,10 @@ window.onscroll = () => {
         if(top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                let activeLink = document.querySelector('header nav a[href*=' + id + ']');
+                if (activeLink) {
+                    activeLink.classList.add('active');
+                }
             });
         };
     });
